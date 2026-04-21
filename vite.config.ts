c24564e721d2c9b98b3a51fe44ef51468f351c05
minhphuongtnut102-cpu <minhphuongtnut102@gmail.com>
2026-04-21 10:23:08 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
-
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: '/cay/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
